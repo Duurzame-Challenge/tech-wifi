4250f49c5f5b5fc564979619fcf8a8fc58e67ef9
Mika <mika.rutjens@student.fontys.nl>
2024-11-08 06:49:04 +0100

    Http request 1

--- ArduinoWifi.ino
+++ ArduinoWifi.ino
@@ -1,10 +1,12 @@
+#include <R4HttpClient.h>
+
 //#include <dummy.h>
 
 //#include <Arduino_DebugUtils.h>
 
 //#include <Arduino_ESP32_OTA.h>
 
-//#include <ArduinoHttpClient.h>
+#include <ArduinoHttpClient.h>
 
 #include <UnoWiFiDevEd.h>
 
@@ -13,11 +15,12 @@
 #include <WiFi.h>
 //#include <HTTPClient.h>
 
-const char* ssid = "Netlab-OIL430";
-const char* password = "DesignChallenge";    
-String serverPath = "https://nickvanhooff.com/DuurzameScannerApi/public/api/products";  // Vervang door je eigen API-URL
+const char* ssid = "Redmi 13C";
+const char* password = "mika_2004";    
+String serverPath = "https://nickvanhooff.com";  // Vervang door je eigen API-URL
 
-
+WiFiClient wifi;
+HttpClient client = HttpClient(wifi, serverPath, 443);
 
 // Definieer het statische IP-adres, subnetmasker en gateway
 IPAddress local_IP(192, 168, 68, 249); // Vervang dit door het gewenste IP-adres
@@ -28,7 +31,7 @@ void setup() {
     Serial.begin(9600);
 
     // Probeer het statische IP-adres in te stellen
-    WiFi.config(local_IP, gateway, subnet);
+   // WiFi.config(local_IP, gateway, subnet);
     // Verbinden met het WiFi-netwerk
     WiFi.begin(ssid, password);
 
@@ -51,5 +54,23 @@ void loop() {
         delay(1000);
          Serial.print("IP adres: ");
         Serial.println(WiFi.localIP());
+        GetRequest();
     }
+}
+
+void GetRequest()
+{
+  Serial.println("making GET request");
+  client.get("/DuurzameScannerApi/public/api/products");
+
+  // read the status code and body of the response
+  int statusCode = client.responseStatusCode();
+  String response = client.responseBody();
+
+  Serial.print("Status code: ");
+  Serial.println(statusCode);
+  Serial.print("Response: ");
+  Serial.println(response);
+  Serial.println("Wait five seconds");
+  delay(5000);
 }
